2ab20c0c21549e51b971965849de3fa6768a55f9
christopherkesuma <dnlkusuma1@gmail.com>
2022-04-26 08:03:31 +0700

    push assets home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 @include('partial.guestNavbar')
-<div style="padding-top:80px; background-image: url({{ asset('storage/frontend/home.jpg') }}); height: 60vh; background-attachment: fixed; background-size: cover; background-position:center">
+<div style="padding-top:80px; background-image: url({{ asset('frontend/home.jpg') }}); height: 60vh; background-attachment: fixed; background-size: cover; background-position:center">
     <div class="col-4 m-auto mb-5" style="background-color: rgba(0,0,0,.4); color: white; text-align:center; font-size:28pt; font-weight: 750">Find Your Future Home</div>
     <form class="d-flex col-11 m-auto" action="" method="GET" style="justify-content: space-between">
         @csrf
@@ -12,19 +12,19 @@
 </div>
 <div class="d-flex mt-5 mb-5" style="justify-content: space-evenly">
     <a href="" class="card" style="width: 18rem; text-decoration: none; color: black; border-radius: 10px" onmouseover="cardOver(this)" onmouseout="cardOut(this)">
-        <img src="{{ asset('storage/frontend/buyicon.jpg') }}" class="card-img-top" alt="..." style="height: 160px; border-top-left-radius: 10px; border-top-right-radius: 10px">
+        <img src="{{ asset('frontend/buyicon.jpg') }}" class="card-img-top" alt="..." style="height: 160px; border-top-left-radius: 10px; border-top-right-radius: 10px">
         <div class="card-body" style="text-align: center">
             <h5 class="card-title">Buy Real Estates</h5>
         </div>
     </a>
     <a href="" class="card" style="width: 18rem; text-decoration: none; color: black; border-radius: 10px" onmouseover="cardOver(this)" onmouseout="cardOut(this)">
-        <img src="{{ asset('storage/frontend/renticon.jpg') }}" class="card-img-top" alt="..." style="height: 160px; border-top-left-radius: 10px; border-top-right-radius: 10px">
+        <img src="{{ asset('frontend/renticon.jpg') }}" class="card-img-top" alt="..." style="height: 160px; border-top-left-radius: 10px; border-top-right-radius: 10px">
         <div class="card-body" style="text-align: center">
             <h5 class="card-title">Rent Real Estates</h5>
         </div>
     </a>
     <a href="" class="card" style="width: 18rem; text-decoration: none; color: black; border-radius: 10px" onmouseover="cardOver(this)" onmouseout="cardOut(this)">
-        <img src="{{ asset('storage/frontend/abouticon.jpg') }}" class="card-img-top" alt="..." style="height: 160px; border-top-left-radius: 10px; border-top-right-radius: 10px">
+        <img src="{{ asset('frontend/abouticon.jpg') }}" class="card-img-top" alt="..." style="height: 160px; border-top-left-radius: 10px; border-top-right-radius: 10px">
         <div class="card-body" style="text-align: center">
             <h5 class="card-title">About Us</h5>
         </div>
--- resources/views/partial/adminNavbar.blade.php
+++ resources/views/partial/adminNavbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #FF9F46">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-        <img src="{{ asset('storage/frontend/logomegawealth.png') }}" alt="logo" style="height: 50px">
+        <img src="{{ asset('frontend/logomegawealth.png') }}" alt="logo" style="height: 50px">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
