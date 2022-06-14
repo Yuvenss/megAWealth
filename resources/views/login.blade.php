7485c64fb45a0f1b3c7bd70de3b01d7908e91345
christopherkesuma <dnlkusuma1@gmail.com>
2022-06-14 12:59:23 +0700

    fix login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -36,7 +36,7 @@
                 @csrf
                 <div class="mb-3">
                     <label for="email" class="form-label">Email Address</label>
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Enter your email address here..." style="border: 1.5px solid #F76F12" value={{ Cookie::get('LoginCookie') !== NULL ? Cookie::get('LoginCookie') : "" }} {{-- value="{{ old('email') }}" --}} autofocus>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Enter your email address here..." style="border: 1.5px solid #F76F12" value="{{ Cookie::get('LoginCookie') !== NULL ? Cookie::get('LoginCookie') : old('email') }}" {{-- value="{{ old('email') }}" --}} autofocus>
                     @error('email')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
